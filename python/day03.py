a3ec761e50c39ddd9d38d2bf4ad1249f83f999f7
Vaclav Lunak <v.lunak@gmail.com>
2023-12-03 09:42:18 +0100

    day 3

---
+++ python/day03.py
@@ -0,0 +1,62 @@
+import re
+from math import prod
+from collections import defaultdict
+
+with open("../inputs/03/input.txt") as f:
+    board = [l.strip() for l in f.readlines()]
+
+board.append('.'*len(board[0]))
+
+def get_symbols(string):
+    return list(re.finditer(r'[^\d.]+', string))
+
+def get_numbers(string):
+    return list(re.finditer(r'\d+', string))
+
+def overlaps(number, symbol):
+    s = symbol.start()
+    if s < number.start() - 1:
+        return -1
+    elif s <= number.end():
+        return 0
+    else:
+        return 1
+
+def find_overlaps(numbers, symbols):
+    i = 0
+    for num in numbers:
+        while i < len(symbols):
+            cmp = overlaps(num, symbols[i])
+            if cmp == 0:
+                yield num, symbols[i]
+                break
+            if cmp == 1:
+                break
+
+def parse_board(board):
+    number_map, symbol_map = defaultdict(set), defaultdict(set)
+    symbols_above = []
+    symbols_on_line = get_symbols(board[0])
+    for line in range(len(board)-1):
+        numbers_on_line = get_numbers(board[line])
+        symbols_below = get_symbols(board[line+1])
+
+        for d, symbols in enumerate((symbols_above, symbols_on_line, symbols_below)):
+            for overlap_n, overlap_s in find_overlaps(numbers_on_line, symbols):
+                num_key = (line + d - 1, overlap_n.start(), int(overlap_n.group()))
+                sym_key = (line + d - 1, overlap_s.start(), overlap_s.group())
+                number_map[num_key].add(sym_key)
+                symbol_map[sym_key].add(num_key)
+
+        symbols_above = symbols_on_line
+        symbols_on_line = symbols_below
+    return number_map, symbol_map
+
+if __name__ == "__main__":
+    numbers, symbols = parse_board(board)
+    total = sum(nk[2] for nk in numbers.keys())
+    print(total)
+
+    gear_ratios = sum(prod(nk[2] for nk in symbols[g]) for g in symbols if g[2] == '*' and len(symbols[g]) == 2)
+    print(gear_ratios)
+
